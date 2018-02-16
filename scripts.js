229beff36ca49061f9c8478b2b6b09a883bf83e3
Petra Frantz <petra17.ru.is>
2018-02-16 18:15:51 +0000

    add files

---
+++ scripts.js
@@ -0,0 +1,110 @@
+
+$('document').ready(function() {
+
+    var tds = $('td');
+    var button = $('#button');
+    var player = "X";
+    var game = "going";
+    var xWin = 0;
+    var oWin = 0;
+
+    function checkGame(mark) {
+
+        if ($('#five').html() == mark) {
+            if ($('#one').html() == mark && $('#nine').html() == mark) {
+                $('p').html(mark + " has won");
+                return "over";
+            }
+            else if ($('#two').html() == mark && $('#eight').html() == mark) {
+                return "over";
+            }
+            else if ($('#three').html() == mark && $('#seven').html() == mark) {
+                return "over";
+            }
+            else if ($('#four').html() == mark && $('#six').html() == mark) {
+                return "over";
+            }
+        }
+
+        else if ($('#seven').html() == mark) {
+            if ($('#eight').html() == mark && $('#nine').html() == mark) {
+                return "over";
+            }
+            else if ($('#one').html() == mark && $('#four').html() == mark) {
+                return "over";
+            }
+        }
+
+        else if ($('#three').html() == mark && $('#six').html() == mark && $('#nine').html() == mark) {
+            return "over";
+        }
+
+        for (i = 0; i < tds.length; i++) {
+            if (tds[i].innerText == "") {
+                return "going";
+            }
+        } 
+
+        return "draw";
+    };
+
+    function switchPlayer() {
+        if (player === "X") {
+         $('h2').html("O, it‘s your turn!");
+            player = "O";
+        }
+        else {
+            $('h2').html("X, it‘s your turn!");
+            player = "X";
+        }
+    }
+
+    function score() {
+        if (player === "X") {
+            xWin = (xWin + 1);
+            $('#x-winner').html("X: " + xWin);
+        }
+        else if (player === "O") {
+            oWin = (oWin + 1);
+            $('#o-winner').html("O: " + oWin);
+        }
+    }
+
+    tds.on('click', function() {
+        if(game == "going") {
+            if ($(this).html() != "X" && $(this).html() != "O") {
+                $(this).html(player);
+                $(this).addClass(player);
+                game = checkGame(player);
+
+                if (game == "over") {
+                    $('h2').html(player + " won!!!");
+                    score();
+                }
+
+                else if (game == "draw") {
+                    $('h2').html("DRAW!!!");
+                }
+
+                else if (game == "going") {
+                    switchPlayer();
+                }
+            }
+        }   
+     });
+
+     button.on('click', function() {
+         game = "going";
+         winner = "";
+         player = "X";
+         $('h2').html("X, it‘s your turn!");
+
+        for (i = 0; i < tds.length; i++) { 
+            tds[i].innerText = ""; 
+        }
+        tds.removeClass(["X"]);
+        tds.removeClass(["O"]);
+     })
+});
+
+
